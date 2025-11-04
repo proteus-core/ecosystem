df4c779ce476a09f61be38fd45c81bea88b31d65
marton bognar <marton.bognar@kuleuven.be>
2025-11-04 16:34:19 +0100

    Build multiple Proteus simulation binaries in Docker

--- install-scripts/proteus.sh
+++ install-scripts/proteus.sh
@@ -3,6 +3,7 @@
 set -ex
 
 git clone --recurse-submodules --branch main https://github.com/proteus-core/proteus.git core
-make -C simulation clean
-make -C simulation CORE=riscv.CoreDynamicExtMem32
-make -C functional-tests CORE=riscv.CoreDynamicExtMem32 BUILD_CORE=0 RISCV_PREFIX=riscv64-unknown-elf
+make -C simulation CORE=riscv.CoreExtMem32 && cp simulation/build/sim simulation/build/sim-32-i
+make -C simulation CORE=riscv.CoreDynamicExtMem32 && cp simulation/build/sim simulation/build/sim-32-o
+make -C simulation CORE=riscv.CoreExtMem64 && cp simulation/build/sim simulation/build/sim-64-i
+make -C simulation CORE=riscv.CoreDynamicExtMem64 && cp simulation/build/sim simulation/build/sim-64-o
