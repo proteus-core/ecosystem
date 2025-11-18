8420b540b06630471acffff449f95592c12e3fe6
marton bognar <marton.bognar@kuleuven.be>
2025-11-18 17:26:20 +0100

    Attempt to save disk space

--- install-scripts/toolchain.sh
+++ install-scripts/toolchain.sh
@@ -7,7 +7,7 @@ set -ex
 ################################################################################
 
 mkdir -p /toolchain && cd /toolchain
-git clone https://github.com/riscv/riscv-gnu-toolchain .
+git clone --depth 1 --shallow-submodules https://github.com/riscv/riscv-gnu-toolchain .
 apt-get -yqq install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build
 ./configure --prefix=/opt/riscv --with-cmodel=medany --with-multilib-generator="rv32im_zicsr_zicond-ilp32--;rv64im_zicsr_zicond-lp64--" --enable-debug-info && make -j$(nproc) && make clean
 echo 'PATH="/opt/riscv/bin:$PATH"' >> /root/.bashrc
