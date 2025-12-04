4ac31787b1c30b5642eec1351509652e5ab6ea8b
marton bognar <marton.bognar@kuleuven.be>
2025-12-05 00:16:58 +0100

    Fix yosys dependencies

--- install-scripts/riscv-formal.sh
+++ install-scripts/riscv-formal.sh
@@ -6,6 +6,7 @@ git clone https://github.com/YosysHQ/riscv-formal.git
 
 git clone --recurse-submodules --depth 1 https://github.com/YosysHQ/yosys.git
 cd yosys
+apt-get install -yqq build-essential clang lld bison flex libreadline-dev gawk tcl-dev libffi-dev git graphviz xdot pkg-config python3 libboost-system-dev libboost-python-dev libboost-filesystem-dev zlib1g-dev
 make -j$(nproc)
 make install
 
