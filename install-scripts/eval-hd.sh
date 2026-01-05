3a6c803ba14ebb7bea3c9a008d6537a8a6ce07e6
marton bognar <martonbognar@users.noreply.github.com>
2026-01-05 16:04:40 +0100

    Use default EVAL-HD scripts and improve tooling (#6)
    * Use original scripts provided in EVAL-HD
    
    * Remove references to absolute paths
    
    * Only deploy Docker image based on the main branch

--- install-scripts/eval-hd.sh
+++ install-scripts/eval-hd.sh
@@ -3,5 +3,5 @@
 set -ex
 
 git clone --recurse-submodules --depth 1 --shallow-submodules https://github.com/KULeuven-COSIC/eval-hd.git
-. /ecosystem/waveform-analysis/.venv/bin/activate
+. "$(dirname "$0")/../waveform-analysis/.venv/bin/activate"
 pip install pyosys
