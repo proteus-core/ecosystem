9021c1b9ac78add0ed47a49dfe560a4bd1c9a288
marton bognar <marton.bognar@kuleuven.be>
2026-07-09 17:10:59 +0200

    Make toolchain installation Docker-independent

--- install-scripts/toolchain.sh
+++ install-scripts/toolchain.sh
@@ -6,9 +6,13 @@ set -ex
 # Install GNU RISC-V toolchain
 ################################################################################
 
-mkdir -p /toolchain && cd /toolchain
-git clone --depth 1 --shallow-submodules https://github.com/riscv/riscv-gnu-toolchain .
-apt-get -yqq install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build
-./configure --prefix=/opt/riscv --with-cmodel=medany --with-multilib-generator="rv32im_zicsr_zicond-ilp32--;rv64im_zicsr_zicond-lp64--" --enable-debug-info && make -j$(nproc) && make clean
-echo 'PATH="/opt/riscv/bin:$PATH"' >> /root/.bashrc
-rm -rf /toolchain
+ROOT_DIR="$(pwd)"
+SUDO=""
+[ "$(id -u)" -eq 0 ] || SUDO="sudo"
+
+mkdir -p toolchain-tmp && cd toolchain-tmp
+git clone --branch 2026.05.06 --depth 1 --shallow-submodules https://github.com/riscv/riscv-gnu-toolchain .
+$SUDO apt-get -yqq install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build
+./configure --prefix="${ROOT_DIR}/gnu" --with-cmodel=medany --with-multilib-generator="rv32im_zicsr_zifencei_zicond-ilp32--;rv64im_zicsr_zifencei_zicond-lp64--" --enable-debug-info && make -j$(nproc) && make clean
+echo "PATH=\"${ROOT_DIR}/gnu/bin:\$PATH\"" >> ~/.bashrc
+cd .. && rm -rf toolchain-tmp
