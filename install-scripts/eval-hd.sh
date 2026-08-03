1c1b049f7d1b0ec846582c523b850bb7749b1304
marton bognar <marton.bognar@kuleuven.be>
2026-08-03 15:51:28 +0200

    Setup fixes

--- install-scripts/eval-hd.sh
+++ install-scripts/eval-hd.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-git clone --recurse-submodules --depth 1 --shallow-submodules https://github.com/KULeuven-COSIC/eval-hd.git
+git clone --recurse-submodules --depth 1 --branch v0.3 --shallow-submodules https://github.com/KULeuven-COSIC/eval-hd.git
 . "$(dirname "$0")/../waveform-analysis/.venv/bin/activate"
 pip install pyosys
--- install-scripts/toolchain.sh
+++ install-scripts/toolchain.sh
@@ -14,6 +14,8 @@ $SUDO apt-get -yqq install git autoconf automake autotools-dev curl python3 libm
 
 mkdir -p toolchain-tmp && cd toolchain-tmp
 git clone --branch 2026.05.06 --depth 1 --shallow-submodules https://github.com/riscv/riscv-gnu-toolchain .
-./configure --prefix="${ROOT_DIR}/gnu" --with-cmodel=medany --with-multilib-generator="rv32im_zicsr_zifencei_zicond-ilp32--;rv64im_zicsr_zifencei_zicond-lp64--" --enable-debug-info && make -j$(nproc) && make clean
+./configure --prefix="${ROOT_DIR}/gnu" --with-cmodel=medany --with-multilib-generator="rv32im_zicsr_zifencei_zicond-ilp32--;rv64im_zicsr_zifencei_zicond-lp64--" --enable-debug-info
+make -j$(nproc)
+make clean
 echo "PATH=\"${ROOT_DIR}/gnu/bin:\$PATH\"" >> ~/.bashrc
 cd .. && rm -rf toolchain-tmp
