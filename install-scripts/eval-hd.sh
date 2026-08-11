2e15dd6f45bedef855560f34e319fd9c4de9e414
marton bognar <marton.bognar@kuleuven.be>
2026-08-11 14:07:52 +0200

    Fix EVAL-HD dependency installation

--- install-scripts/eval-hd.sh
+++ install-scripts/eval-hd.sh
@@ -4,4 +4,4 @@ set -ex
 
 git clone --recurse-submodules --depth 1 --branch v0.3 --shallow-submodules https://github.com/KULeuven-COSIC/eval-hd.git
 . "$(dirname "$0")/../waveform-analysis/.venv/bin/activate"
-pip install pyosys
+pip install -r eval-hd/requirements.txt
