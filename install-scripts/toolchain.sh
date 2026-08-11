234606162bd013dbd7b703459faab2b2ed606e1e
marton bognar <marton.bognar@kuleuven.be>
2026-08-11 13:54:50 +0200

    Temporary toolchain build workaround

--- install-scripts/toolchain.sh
+++ install-scripts/toolchain.sh
@@ -14,6 +14,14 @@ $SUDO apt-get -yqq install git autoconf automake autotools-dev curl python3 libm
 
 mkdir -p toolchain-tmp && cd toolchain-tmp
 git clone --branch 2026.05.06 --depth 1 --shallow-submodules https://github.com/riscv/riscv-gnu-toolchain .
+
+# temporary fix to pull submodules
+sed -i \
+  -e 's#url = https://sourceware.org/git/binutils-gdb.git#url = git://sourceware.org/git/binutils-gdb.git#' \
+  -e 's#url = https://sourceware.org/git/glibc.git#url = git://sourceware.org/git/glibc.git#' \
+  -e 's#url = https://sourceware.org/git/newlib-cygwin.git#url = git://sourceware.org/git/newlib-cygwin.git#' \
+  .gitmodules
+
 ./configure --prefix="${ROOT_DIR}/gnu" --with-cmodel=medany --with-multilib-generator="rv32im_zicsr_zifencei_zicond-ilp32--;rv64im_zicsr_zifencei_zicond-lp64--" --enable-debug-info
 make -j$(nproc)
 make clean
