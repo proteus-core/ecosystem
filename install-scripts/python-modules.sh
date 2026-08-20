cd2b996b46cfd8ef55f61d8eb475e9614e4bf31f
marton bognar <marton.bognar@kuleuven.be>
2026-08-20 14:28:43 +0200

    Path fix

--- install-scripts/python-modules.sh
+++ install-scripts/python-modules.sh
@@ -4,5 +4,6 @@ set -ex
 
 cd "$(dirname "$0")/.."
 python3 -m venv .python-venv
+cd waveform-analysis
 # build module from waveform-analysis
-.python-venv/bin/pip install -e .
+../.python-venv/bin/pip install -e .
